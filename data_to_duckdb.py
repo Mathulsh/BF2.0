5a88b7d09618b4e8d3dc2a4db7d620eb76cbd2be
Mathulsh <Mathulsh@buaa.edu.cn>
2026-04-07 13:06:45 +0800

    BF2.0.1

--- data_to_duckdb.py
+++ data_to_duckdb.py
@@ -1,11 +1,13 @@
-from rd import collect_redis_results_to_duckdb, redis_list
+from rd import collect_redis_results_to_duckdb_parallel, redis_list
 
-collect_redis_results_to_duckdb(
+collect_redis_results_to_duckdb_parallel(
     # redis_host=redis_host,
     # redis_port=redis_port,
     # redis_password=redis_password,
     redis_list=redis_list,
     duckdb_path="results.duckdb",
-    batch_size=5000,          # ✅ 降低批量大小（很关键）
+    batch_size=5000,
     idle_timeout=30,
+    stop_event=None,
+    check_interval=0.05,
 )
